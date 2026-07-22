64507e07112f52a68305e4290699071cec2bd726
Minea521 <chanmineachea@gmail.com>
2026-07-22 14:45:24 +0700

    Finish offline mode

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   app.enableCors({
-    origin: [process.env.FRONTEND_URL || 'http://localhost:9000', 'http://localhost:9200'],
+    origin: [process.env.FRONTEND_URL || 'http://localhost:9000', 'http://localhost:9200', 'http://localhost:4000'],
     methods: 'GET,POST,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
